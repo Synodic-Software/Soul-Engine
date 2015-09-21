2f61c0fce3d40ff3a6454d680945688cb3b2581c
Behemyth <asher.norland@gmail.com>
2015-09-21 17:48:04 -0400

    fix

--- Soul Engine/Source Files/Ray Engine/RayJob.h
+++ Soul Engine/Source Files/Ray Engine/RayJob.h
@@ -4,12 +4,14 @@
 #include "Utility\CUDA\CUDAManaged.cu"
 
 enum castType{ RayCOLOUR, RayDISTANCE, RayOBJECT_ID, RayNORMAL, RayUV };
+#define INFINITY 99999999999999.999999999999f
 
 class RayJob : public Managed{
 public:
 
-	//Takes an array of desired outputs, its size, the function that decides the ray generation, and the number of rays to shoot.
-	RayJob(castType[], uint, ,uint);
+	//Takes an array of desired outputs, its size, the function that decides the ray generation, and the number of rays to shoot. 
+	//The last parameter is the speed of the ray
+	RayJob(castType[], uint, ,uint, float);
 private:
 
 	castType* type;
