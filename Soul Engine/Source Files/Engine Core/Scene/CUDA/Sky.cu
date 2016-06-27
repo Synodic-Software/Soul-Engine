d8b040702b64cb5af6029e1bdbc81192c95f2824
Behemyth <asher.norland@gmail.com>
2016-06-26 19:41:07 -0500

    removed content (sky stuff)

--- Soul Engine/Source Files/Engine Core/Scene/CUDA/Sky.cu
+++ Soul Engine/Source Files/Engine Core/Scene/CUDA/Sky.cu
@@ -2,15 +2,19 @@
 
 Sky::Sky(std::string texName) {
 
-	image = new Image();
+	//image = new Image();
 
-	CudaCheck(cudaDeviceSynchronize());
-	image->LoadFromFile(texName.c_str(),true,false);
-	CudaCheck(cudaDeviceSynchronize());
+	//CudaCheck(cudaDeviceSynchronize());
+	//image->LoadFromFile(texName.c_str(),true,false);
+	//CudaCheck(cudaDeviceSynchronize());
 }
 
 __device__ glm::vec3 Sky::ExtractColour(const glm::vec3& direction){
-	float4 col = tex2D<float4>(image->texObj, 0.5f + atan2f(direction.z, direction.x) / (2 * PI), 0.5f - asinf(direction.y)/PI);
 
-	return glm::vec3(col.x, col.y, col.z);
+	/*float theta = 0.5f + atan2f(direction.z, direction.x)/(2 * PI);
+	float gamma = 0.5f - asinf(direction.y)/ PI;
+	float4 col = tex2D<float4>(image->texObj, theta, gamma );*/
+
+
+	return glm::vec3(135 / 255.0f, 135 / 255.0f, 230 / 255.0f);
 }
