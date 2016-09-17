85dc97ba5a3e5f5ce03cec8207c206e19aa5a46c
syo <hadlec@rpi.edu>
2016-09-16 20:19:18 -0400

    added implementation of quicksort, not directed for GPU yet

--- Soul Engine/Source Files/Algorithms/Data Algorithms/GPU Quicksort/QuickSort.cpp
+++ Soul Engine/Source Files/Algorithms/Data Algorithms/GPU Quicksort/QuickSort.cpp
@@ -26,6 +26,37 @@ typedef struct qsortRingbuf_t
 #define QSORT_STACK_ELEMS   1*1024*1024 // One million stack elements is a HUGE number.
 
 /*
+void quicksort(T* data, int N)
+{
+  int i, j;
+  T v, t;
+ 
+  if( N <= 1 )
+    return;
+ 
+  // Partition elements
+  v = data[0];
+  i = 0;
+  j = N;
+  for(;;)
+  {
+    while(data[++i] < v && i < N) { }
+    while(data[--j] > v) { }
+    if( i >= j )
+      break;
+    t = data[i];
+    data[i] = data[j];
+    data[j] = t;
+  }
+  t = data[i-1];
+  data[i-1] = data[0];
+  data[0] = t;
+  quicksort(data, i-1);
+  quicksort(data+i, N-i);
+}
+*/
+
+/*
 
 Quicksort(A as array, low as int, high as int){
     if (low < high){
