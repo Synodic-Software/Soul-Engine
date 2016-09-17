e3a5723a83c30ce18eae8bd08d55dcf504c6977b
syo <hadlec@rpi.edu>
2016-09-16 21:02:03 -0400

    reworked iskeyavailable function to be a lot simpler

--- Soul Engine/Source Files/Input/Input.cpp
+++ Soul Engine/Source Files/Input/Input.cpp
@@ -4,13 +4,9 @@ std::list<std::function<void()> > keyHash[350];
 GLFWwindow* window;
 
 bool IsKeyAvailable(int key){
-	if (keyHash[key].size() == 0){
-		return true;
-	}
-	else{
-		return false;
-	}
+	return keyHash[key].size() == 0;
 }
+
 void InputKeyboardCallback(GLFWwindow* window, int key, int scancode, int action, int mods){
 	if (!IsKeyAvailable(key)){
 		for (std::list<std::function<void()> >::iterator itr = keyHash[key].begin(); itr != keyHash[key].end(); itr++){
@@ -23,8 +19,7 @@ bool SetKey(int key, std::function<void()> function){
 	//if (IsKeyAvailable(key)){
 		keyHash[key].push_back(function);
 		return true;
-	//}
-	//else {
+	//} else {
 	//	return false;
 	//}
 }
