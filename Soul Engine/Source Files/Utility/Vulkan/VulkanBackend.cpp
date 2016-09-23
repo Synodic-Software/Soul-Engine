243448b3c9ae65876c6db53e771db222580cdc6f
Behemyth <asher.norland@gmail.com>
2016-09-23 14:26:02 -0400

    Set vertices

--- Soul Engine/Source Files/Utility/Vulkan/VulkanBackend.cpp
+++ Soul Engine/Source Files/Utility/Vulkan/VulkanBackend.cpp
@@ -688,43 +688,6 @@ void VulkanBackend::CopyImage(VkImage srcImage, VkImage dstImage, uint32_t width
 	EndSingleTimeCommands(commandBuffer);
 }
 
-void VulkanBackend::LoadModel() {
-	tinyobj::attrib_t attrib;
-	std::vector<tinyobj::shape_t> shapes;
-	std::vector<tinyobj::material_t> materials;
-	std::string err;
-
-	if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &err, MODEL_PATH.c_str())) {
-		throw std::runtime_error(err);
-	}
-
-	std::unordered_map<VulkanVertex, int> uniqueVertices = {};
-
-	for (const auto& shape : shapes) {
-		for (const auto& index : shape.mesh.indices) {
-			VulkanVertex vertex = {};
-
-			vertex.pos = {
-				attrib.vertices[3 * index.vertex_index + 0],
-				attrib.vertices[3 * index.vertex_index + 1],
-				attrib.vertices[3 * index.vertex_index + 2]
-			};
-
-			vertex.texCoord = {
-				attrib.texcoords[2 * index.texcoord_index + 0],
-				1.0f - attrib.texcoords[2 * index.texcoord_index + 1]
-			};
-
-			if (uniqueVertices.count(vertex) == 0) {
-				uniqueVertices[vertex] = vertices.size();
-				vertices.push_back(vertex);
-			}
-
-			indices.push_back(uniqueVertices[vertex]);
-		}
-	}
-}
-
 void VulkanBackend::CreateVertexBuffer() {
 	VkDeviceSize bufferSize = sizeof(vertices[0]) * vertices.size();
 
--- Soul Engine/Source Files/Utility/Vulkan/VulkanBackend.h
+++ Soul Engine/Source Files/Utility/Vulkan/VulkanBackend.h
@@ -115,7 +115,16 @@ private:
 
 
 
+	const std::vector<VulkanVertex> vertices = {
+		{ { -0.5f, -0.5f, 0.0f }, { 1.0f, 0.0f, 0.0f }, { 0.0f, 0.0f } },
+		{ { 0.5f, -0.5f, 0.0f }, { 0.0f, 1.0f, 0.0f }, { 1.0f, 0.0f } },
+		{ { 0.5f, 0.5f, 0.0f }, { 0.0f, 0.0f, 1.0f }, { 1.0f, 1.0f } },
+		{ { -0.5f, 0.5f, 0.0f }, { 1.0f, 1.0f, 1.0f }, { 0.0f, 1.0f } },
+	};
 
+	const std::vector<uint16_t> indices = {
+		0, 1, 2, 2, 3, 0,
+	};
 
 
 
