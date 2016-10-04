d45b6d06e438975ba7da87a608b3f9ea7fd43892
ferria <ferria@rpi.edu>
2016-10-04 17:03:49 -0400

    Primary Constructor Specified

--- Soul Engine/Source Files/Shaders/Shader.h
+++ Soul Engine/Source Files/Shaders/Shader.h
@@ -33,6 +33,37 @@ public:
 	Shader();
 	
 	//Initialize Shader from argument provided data
+
+	/*
+
+	Shader Constructor takes the following arguments:
+
+	glm::vec4 storage: the energy of the light a ray is carrying
+
+	glm::vec3 direction: the direction the ray is traveling in
+
+	glm::vec3 origin the origin of the ray
+
+	void * material: holds arbitrary material info
+	(Note that void* is just a temporary type holder
+	and will eventually be updated to a proper material 
+	class's pointer)
+
+	glm::vec2 hitCoord: barycentric hit coordinate
+
+	
+	The following arguments all deal with a face to be hit:
+
+	glm::vec3 pos1, pos2, pos2: the positions of the face
+
+	glm::vec3 norm1, norm2, norm3: the normals of the face
+
+	glm::vec3 tex1, tex2, tex3: the texture coordinates of the face
+	
+	Returns:
+	A new Shader object initialized with the above values
+	*/
+
 	Shader(glm::vec4 & storage, glm::vec3 & direction, glm::vec3 & origin, 
 		glm::vec2 & hitCoord, const glm::vec3 & p1, const glm::vec3 & p2,
 		const glm::vec3 & p3, const glm::vec3 & norm1, const glm::vec3 & norm2, const glm::vec3 & norm3,
@@ -43,6 +74,9 @@ public:
 	
 	//Destructor
 	~Shader();
+
+
+
 
 	//Member Functions
 
