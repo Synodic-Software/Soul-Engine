49ccb0cebff94c85754e81ad35878a9a2c3871fb
ferria <ferria@rpi.edu>
2016-10-04 16:44:54 -0400

    Added Class Description Comments

--- Soul Engine/Source Files/Shaders/Shader.h
+++ Soul Engine/Source Files/Shaders/Shader.h
@@ -4,6 +4,26 @@
 #include "SoulCore.h"
 #include "glm\glm.hpp"
 
+
+/*
+
+Class Shader:
+
+Shader is an Abstract Base Class for all other shaders to inheret from.
+
+This class specifies basic inputs and outputs for several functions which
+are common across all shader implementations.
+
+
+
+If you are implementing a new shader type, have it inherit from this class.
+
+*/
+
+
+
+
+
 class Shader {
 public:
 
@@ -26,7 +46,16 @@ public:
 
 	//Member Functions
 
+	//Returns whether or not a Face was hit by a Ray
+	virtual void isFaceHit() = 0;
+
+
+
+
+//Member Variables
 protected:
+
+	//Initialized at Object Creation
 	glm::vec4 &storage;
 	glm::vec3 &direction, &origin;
 	glm::vec2 &hitCoord;
@@ -35,4 +64,3 @@ protected:
 	const glm::vec2 &tex1, &tex2, &tex3;
 	void *material;
 };
-
