419bcf135e1dc23bc7ebc8d356507a5179cd8e69
ferria <ferria@rpi.edu>
2016-10-04 16:33:16 -0400

    Added new constructor types

--- Soul Engine/Source Files/Shaders/Shader.h
+++ Soul Engine/Source Files/Shaders/Shader.h
@@ -7,13 +7,24 @@
 class Shader {
 public:
 
-	//Member Functions
+	//Constructors and Destructors
+
+	//Default Constructor
+	Shader();
+	
+	//Initialize Shader from argument provided data
 	Shader(glm::vec4 & storage, glm::vec3 & direction, glm::vec3 & origin, 
 		glm::vec2 & hitCoord, const glm::vec3 & p1, const glm::vec3 & p2,
 		const glm::vec3 & p3, const glm::vec3 & norm1, const glm::vec3 & norm2, const glm::vec3 & norm3,
 		const glm::vec2 & tex1, const glm::vec2 & tex2, const glm::vec2 & tex3, void* material);
+
+	//Copy Constructor
+	Shader(const Shader & other);
 	
+	//Destructor
 	~Shader();
+
+	//Member Functions
 
 protected:
 	glm::vec4 &storage;
