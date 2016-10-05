c60aa55a4f380e5f1d14f89abc047969463163d6
Behemyth <asher.norland@gmail.com>
2016-10-05 01:00:31 -0400

    getting there

--- Soul Engine/Source Files/Input/Input.cpp
+++ Soul Engine/Source Files/Input/Input.cpp
@@ -1,5 +1,7 @@
 #include "Input.h"
 
+
+
 std::list<std::function<void()> > keyHash[350];
 GLFWwindow* window;
 
@@ -7,7 +9,7 @@ bool IsKeyAvailable(int key){
 	return keyHash[key].size() == 0;
 }
 
-void InputKeyboardCallback(GLFWwindow* window, int key, int scancode, int action, int mods){
+void SoulInput::InputKeyboardCallback(GLFWwindow* window, int key, int scancode, int action, int mods){
 	if (!IsKeyAvailable(key)){
 		for (std::list<std::function<void()> >::iterator itr = keyHash[key].begin(); itr != keyHash[key].end(); itr++){
 		(*itr)();
@@ -15,14 +17,26 @@ void InputKeyboardCallback(GLFWwindow* window, int key, int scancode, int action
 	}
 }
 
-bool SetKey(int key, std::function<void()> function){
+bool SoulInput::SetKey(int key, std::function<void()> function){
 	//doesn't currently check for availability, this is bad - talk to me about whats needed here
 	keyHash[key].push_back(function);
 	return true;
 }
-void SetInputWindow(GLFWwindow* windowN){
+void SoulInput::SetInputWindow(GLFWwindow* windowN){
 	window = windowN;
 }
-GLFWwindow* GetInputWindow(){
+GLFWwindow* SoulInput::GetInputWindow(){
 	return window;
+}
+
+void SoulInput::UpdateMouseCallback(GLFWwindow* window, double xpos, double ypos)
+{
+	int width, height;
+	glfwGetWindowSize(window, &width, &height);
+	xPos = xpos - (width / 2.0);
+	yPos = ypos - (height / 2.0);
+
+	if (ResetMouse){
+		glfwSetCursorPos(window, width / 2.0f, height / 2.0f);
+	}
 }
--- Soul Engine/Source Files/Input/Input.h
+++ Soul Engine/Source Files/Input/Input.h
@@ -1,8 +1,16 @@
 #pragma once
 
 #include "Engine Core/BasicDependencies.h"
+#include <GLFW/glfw3.h>
 
+namespace SoulInput{
 	bool SetKey(int, std::function<void()>);
 	void InputKeyboardCallback(GLFWwindow*, int, int, int, int);
 	void SetInputWindow(GLFWwindow*);
 	GLFWwindow* GetInputWindow();
+	void UpdateMouseCallback(GLFWwindow*, double, double);
+
+	bool ResetMouse = false;
+	double xPos=0.0;
+	double yPos=0.0;
+}
