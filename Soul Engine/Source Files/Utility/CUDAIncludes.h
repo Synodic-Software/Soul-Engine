37cc254717c2169c1b555afeb2b68496a6baa24d
Behemyth <asher.norland@gmail.com>
2016-10-07 15:23:00 -0400

    removed OpenGL completly

--- Soul Engine/Source Files/Utility/CUDAIncludes.h
+++ Soul Engine/Source Files/Utility/CUDAIncludes.h
@@ -5,7 +5,6 @@
 
 #include "Utility\GLMIncludes.h"
 //#include <cuda_runtime.h>
-#include <cuda_gl_interop.h>
 #include <device_launch_parameters.h>
 #include <device_functions.h>
 #include <curand.h>
