115ee07e1eed863b65896e0f403fb1621e869472
ferria <ferria@rpi.edu>
2016-10-17 11:05:09 -0400

    Fixed Material Ptr type

--- Soul Engine/Source Files/Shaders/Shader.cpp
+++ Soul Engine/Source Files/Shaders/Shader.cpp
@@ -4,7 +4,7 @@
 Shader::Shader(glm::vec4 & storage, glm::vec4 & direction, glm::vec3 & origin,
 	glm::vec2 & hitCoord, const glm::vec3 & p1, const glm::vec3 & p2,
 	const glm::vec3 & p3, const glm::vec3 & norm1, const glm::vec3 & norm2, const glm::vec3 & norm3,
-	const glm::vec2 & tex1, const glm::vec2 & tex2, const glm::vec2 & tex3, void* material) {
+	const glm::vec2 & tex1, const glm::vec2 & tex2, const glm::vec2 & tex3, Material * material) {
 }
 
 
