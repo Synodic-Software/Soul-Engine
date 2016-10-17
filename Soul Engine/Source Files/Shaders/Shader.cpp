a2b8f89167a6b225a1b7044b3d53fba21e4c2fd7
ferria <ferria@rpi.edu>
2016-10-17 11:02:47 -0400

    Fixed Ray Direction type

--- Soul Engine/Source Files/Shaders/Shader.cpp
+++ Soul Engine/Source Files/Shaders/Shader.cpp
@@ -1,7 +1,7 @@
 #include "Shader.h"
 
 
-Shader::Shader(glm::vec4 & storage, glm::vec3 & direction, glm::vec3 & origin,
+Shader::Shader(glm::vec4 & storage, glm::vec4 & direction, glm::vec3 & origin,
 	glm::vec2 & hitCoord, const glm::vec3 & p1, const glm::vec3 & p2,
 	const glm::vec3 & p3, const glm::vec3 & norm1, const glm::vec3 & norm2, const glm::vec3 & norm3,
 	const glm::vec2 & tex1, const glm::vec2 & tex2, const glm::vec2 & tex3, void* material) {
