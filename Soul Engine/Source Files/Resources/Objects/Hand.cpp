aaafe02067ac77004f746b5f424e372539a9b9a5
syo <hadlec@rpi.edu>
2016-10-25 16:12:06 -0400

    documentation stuff

--- Soul Engine/Source Files/Resources/Objects/Hand.cpp
+++ Soul Engine/Source Files/Resources/Objects/Hand.cpp
@@ -7,23 +7,30 @@ objects were originally hardcoded in
 this is now *probably* unneeded
 gonna leave it just in case
 */
+
+//constructor for hand
 Hand::Hand(glm::vec3 it)
 {
 	xyzPosition = it;
 	Load();
 }
 
-
+//destructor for hand
 Hand::~Hand()
 {
 }
 
+//update position
 void Hand::Update(double){
 
 }
+
+//update position late
 void Hand::UpdateLate(double){
 
 }
+
+//load a model for the hand
 void Hand::Load(){
 	ExtractFromFile("lost_empire.obj");
 }
