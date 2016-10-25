16546dad4773dd7934b10842d658477c270c4f50
syo <hadlec@rpi.edu>
2016-10-25 16:11:09 -0400

    documentation stuff

--- Soul Engine/Source Files/Resources/Objects/Hand.cpp
+++ Soul Engine/Source Files/Resources/Objects/Hand.cpp
@@ -1,6 +1,12 @@
 #include "Hand.h"
 //deal with hand model in soul engine
-
+/*
+this is now deprecated
+from a point early in dev
+objects were originally hardcoded in 
+this is now *probably* unneeded
+gonna leave it just in case
+*/
 Hand::Hand(glm::vec3 it)
 {
 	xyzPosition = it;
