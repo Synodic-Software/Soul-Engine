c02cd543411fd7b782b529af6ca1065d991dea94
syo <hadlec@rpi.edu>
2016-10-25 16:28:52 -0400

    added commenting for PhysicsEngine.cpp

--- Soul Engine/Source Files/Physics Engine/PhysicsEngine.cpp
+++ Soul Engine/Source Files/Physics Engine/PhysicsEngine.cpp
@@ -1,7 +1,7 @@
 #include "PhysicsEngine.h"
 #include "CUDA/PhysicsEngine.cuh"
 
-
+//this just processes a scene with the CUDA physics engine
 void PhysicsEngine::Process(const Scene* scene){
 
 	ProcessScene(scene);
