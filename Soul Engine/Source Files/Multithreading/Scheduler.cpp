df96118643044a928c66293b6942c26bececbfa3
Behemyth <asher.norland@gmail.com>
2016-12-16 01:51:15 -0500

    half-modified state (I should really branch)

--- Soul Engine/Source Files/Multithreading/Scheduler.cpp
+++ Soul Engine/Source Files/Multithreading/Scheduler.cpp
@@ -1,12 +1,10 @@
 #include "Scheduler.h"
-#include <boost/fiber/all.hpp>
 #include <thread>
-#include <condition_variable>
 
-std::thread* threads;
-unsigned int threadCount;
 
-static std::size_t fiberCount{ 0 };
+static std::thread* threads;
+static std::size_t threadCount{ 0 };
+
 static bool shouldRun{ true };
 
 static std::mutex fiberMutex{};
@@ -16,47 +14,47 @@ void ThreadRun() {
 	boost::fibers::use_scheduling_algorithm< boost::fibers::algo::shared_work >();
 
 	std::unique_lock<std::mutex> lock(fiberMutex);
-	condition.wait(lock, []() { return 0 == fiberCount && !shouldRun; });
+	condition.wait(lock, []() { return 0 == Scheduler::detail::fiberCount && !shouldRun; });
 }
 
 namespace Scheduler {
 
+	namespace detail {
+		std::size_t fiberCount =0;
+
+
+	}
+
 	void Terminate() {
-
-
 		std::unique_lock<std::mutex> lock(fiberMutex);
 		shouldRun = false;
-		if (0 == --fiberCount) {
+		if (0 == --detail::fiberCount) {
 			lock.unlock();
 			condition.notify_all(); //notify all fibers waiting 
 		}
 
-		while (fiberCount!=0) {
+		while (detail::fiberCount!=0) {
 			boost::this_fiber::yield();
 		}
-
-		for (int i = 0; i < threadCount; ++i) {
+		 
+		for (uint i = 0; i < threadCount; ++i) {
 			threads[i].join();
 		}
 
 		delete[] threads;
-
 	}
 
 	void Init() {
-
 		boost::fibers::use_scheduling_algorithm< boost::fibers::algo::shared_work >();
 
 		threadCount = std::thread::hardware_concurrency() - 1;  //the main thread takes up one slot.
-
 		threads = new std::thread[threadCount];
 
-		fiberCount++;
+		detail::fiberCount++;
 
-		for (int i = 0; i < threadCount; ++i) {
+		for (uint i = 0; i < threadCount; ++i) {
 			threads[i] = std::thread(ThreadRun);
 		}
-
 	}
 
-}
+};
