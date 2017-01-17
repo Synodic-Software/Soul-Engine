fccd1398a6ad2e37c86f149c6ef974f93c85e061
Behemyth <asher.norland@gmail.com>
2017-01-17 12:50:07 -0500

    Added render widget

---
+++ Soul Engine/Source Files/Display/Widget/RenderWidget.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "Display\Widget\Widget.h"
+#include "Metrics.h"
+
+class RenderWidget : public Widget
+{
+public:
+	RenderWidget();
+	~RenderWidget();
+
+protected:
+
+
+
+private:
+
+};
+
