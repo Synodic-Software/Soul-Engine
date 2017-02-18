8cc28a6eac1fb36909bd6404798824efad550498
Behemyth <asher.norland@gmail.com>
2017-02-17 23:50:54 -0500

    More structural files

---
+++ Source Files/Raster Engine/Vulkan/VulkanShader.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <string>
+
+#include "Raster Engine\Shader.h"
+
+class VulkanShader:public Shader {
+public:
+	VulkanShader(const std::string&, std::string, shader_t);
+
+private:
+
+};
