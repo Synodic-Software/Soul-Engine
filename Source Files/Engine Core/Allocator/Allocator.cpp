36aabc778d6f7e4f4d742981ad2ab6585af4d02c
Louis Silvestro <silvel2@rpi.edu>
2017-02-28 16:57:50 -0500

    Add starter implementation for Allocator base class

--- Source Files/Engine Core/Allocator/Allocator.cpp
+++ Source Files/Engine Core/Allocator/Allocator.cpp
@@ -1,9 +1,13 @@
-#pragma once
-#include <cassert>
+#include<"Allocator.h">
+Allocator::Allocator(size_t size, void* start) {
+	_start = start;
+	_capacity = size;
+	_usedMem = 0;
+	_numAllocs = 0;
+}
 
-class Allocator
-{
-
-
-
-};
+Allocator::~Allocator() {
+	assert(usedMem == 0 && numAllocs == 0);
+	_start = nullptr;
+	_capacity = 0;
+}
