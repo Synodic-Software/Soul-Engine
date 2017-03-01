3ab0da064dcbdccd5ef0f108f3b095d26b1f3903
ferria <ferria@rpi.edu>
2017-03-01 15:48:01 -0500

    Added More tests, now supports more archive types

--- Soul Engine/Source Files/Utility/Settings.cpp
+++ Soul Engine/Source Files/Utility/Settings.cpp
@@ -5,7 +5,16 @@
 //Only needed for debugging
 #include <cassert>
 
+//Will try to minimize size of boost variants.  May have a negative impact on performance.
+//For more info see the boost::variant documentation
 #define BOOST_VARIANT_MINIMIZE_SIZE
+
+
+/*
+	NOTE: See header file "Settings.h" for descriptions of all methods.
+*/
+
+
 
 static std::string filename;
 static internal::TableWrapper tableWrapper;
@@ -21,8 +30,7 @@ namespace internal{
 		if (itr == dict.end()) return std::make_pair(defaultValue, false);
 		try {
 			return std::make_pair(boost::relaxed_get<T>(itr->second), true);
-		}
-		catch (boost::bad_get e) {
+		} catch (boost::bad_get e) {
 			return std::make_pair(defaultValue, false);
 		}
 
@@ -85,22 +93,29 @@ namespace internal{
 		ar & boost::serialization::make_nvp("EightByteTypes", this->ebt);
 	}
 
+	template <class T>
 	void TableWrapper::Read(const std::string & _filename){
 		filename = _filename;
 		std::ifstream ifs(filename);
-		boost::archive::text_iarchive ar(ifs);
+		T ar(ifs);
+		//boost::archive::text_iarchive ar(ifs);
 		Table* ptr = new Table();
 		ar & boost::serialization::make_nvp("Table", *ptr);
 		if (this->table != nullptr) delete this->table;
 		this->table = ptr;
 	}
 
+	template <class T>
 	void TableWrapper::Write() {
 		std::ofstream ofs(filename);
-		boost::archive::text_oarchive ar(ofs);
+		T ar(ofs);
+		//boost::archive::text_oarchive ar(ofs);
 		ar & boost::serialization::make_nvp("Table", *(this->table));
 	}
 
+//Tests
+
+//Tests to make sure settings works properly when given proper inputs
 
 	void testGetProperInputs() {
 		assert(Settings::Set("a", std::int8_t(0)));
@@ -126,7 +141,50 @@ namespace internal{
 		assert(Settings::Get("j", 10.1) == 9.9);
 	}
 
-	void testGetNonDefinedSettings() {
+	void testGetProperInputs2() {
+		assert(Settings::Set("a", std::int8_t(0)));
+		assert(Settings::Set("b", std::uint8_t(1)));
+		assert(Settings::Set("c", std::int16_t(2)));
+		assert(Settings::Set("d", std::uint16_t(3)));
+		assert(Settings::Set("e", std::int32_t(4)));
+		assert(Settings::Set("f", std::uint32_t(5)));
+		assert(Settings::Set("g", float(6.6)));
+		assert(Settings::Set("h", std::int64_t(7)));
+		assert(Settings::Set("i", std::uint64_t(8)));
+		assert(Settings::Set("j", 9.9));
+
+		std::int8_t  a; std::uint8_t  b;
+		std::int16_t c; std::uint16_t d;
+		std::int32_t e; std::uint32_t f; float  g;
+		std::int64_t h; std::uint64_t i; double j;
+
+		assert(Settings::Get("a", std::int8_t(1), &a));
+		assert(Settings::Get("b", std::uint8_t(2), &b));
+		assert(Settings::Get("c", std::int16_t(3), &c));
+		assert(Settings::Get("d", std::uint16_t(4), &d));
+		assert(Settings::Get("e", std::int32_t(5), &e));
+		assert(Settings::Get("f", std::uint32_t(6), &f));
+		assert(Settings::Get("g", float(7.7), &g));
+		assert(Settings::Get("h", std::int64_t(8), &h));
+		assert(Settings::Get("i", std::uint64_t(9), &i));
+		assert(Settings::Get("j", 10.1, &j));
+
+		assert(a == std::int8_t(0));
+		assert(b == std::uint8_t(1));
+		assert(c == std::int16_t(2));
+		assert(d == std::uint16_t(3));
+		assert(e == std::int32_t(4));
+		assert(f == std::uint32_t(5));
+		assert(g == 6.6f);
+		assert(h == std::int64_t(7));
+		assert(i == std::uint64_t(8));
+		assert(j == 9.9);
+	}
+
+//Tests to make sure defaultValue used when an undefined 
+//setting is retrived
+
+	void testGetUndefinedSettings() {
 		assert(Settings::Set("a", std::int8_t(0)));
 		assert(Settings::Set("b", std::uint8_t(1)));
 		assert(Settings::Set("c", std::int16_t(2)));
@@ -150,6 +208,49 @@ namespace internal{
 		assert(Settings::Get("a", 10.1) == 10.1);
 	}
 
+	void testGetUndefinedSettings2() {
+		assert(Settings::Set("a", std::int8_t(0)));
+		assert(Settings::Set("b", std::uint8_t(1)));
+		assert(Settings::Set("c", std::int16_t(2)));
+		assert(Settings::Set("d", std::uint16_t(3)));
+		assert(Settings::Set("e", std::int32_t(4)));
+		assert(Settings::Set("f", std::uint32_t(5)));
+		assert(Settings::Set("g", float(6.6)));
+		assert(Settings::Set("h", std::int64_t(7)));
+		assert(Settings::Set("i", std::uint64_t(8)));
+		assert(Settings::Set("j", 9.9));
+
+		std::int8_t  a; std::uint8_t  b;
+		std::int16_t c; std::uint16_t d;
+		std::int32_t e; std::uint32_t f; float  g;
+		std::int64_t h; std::uint64_t i; double j;
+
+		assert(!Settings::Get("j", std::int8_t(1), &a));
+		assert(!Settings::Get("i", std::uint8_t(2), &b));
+		assert(!Settings::Get("h", std::int16_t(3), &c));
+		assert(!Settings::Get("g", std::uint16_t(4), &d));
+		assert(!Settings::Get("f", std::int32_t(5), &e));
+		assert(!Settings::Get("e", std::uint32_t(6), &f));
+		assert(!Settings::Get("d", float(7.7), &g));
+		assert(!Settings::Get("c", std::int64_t(8), &h));
+		assert(!Settings::Get("b", std::uint64_t(9), &i));
+		assert(!Settings::Get("a", 10.1, &j));
+
+		assert(a == std::int8_t(1));
+		assert(b == std::uint8_t(2));
+		assert(c == std::int16_t(3));
+		assert(d == std::uint16_t(4));
+		assert(e == std::int32_t(5));
+		assert(f == std::uint32_t(6));
+		assert(g == 7.7f);
+		assert(h == std::int64_t(8));
+		assert(i == std::uint64_t(9));
+		assert(j == 10.1);
+	}
+
+//Serialization Tests
+
+	template <class T1, class T2>
 	void testSerialization() {
 		assert(Settings::Set("a", std::int8_t(0)));
 		assert(Settings::Set("b", std::uint8_t(1)));
@@ -163,8 +264,36 @@ namespace internal{
 		assert(Settings::Set("j", 9.9));
 
 		Settings::SetFilename("b.ini");
-		Settings::Write();
-		Settings::Read("b.ini");
+		Settings::Write<T2>();
+		Settings::Read<T1>("b.ini");
+
+		assert(Settings::Get("a", std::int8_t(1)) == 0);
+		assert(Settings::Get("b", std::uint8_t(2)) == 1);
+		assert(Settings::Get("c", std::int16_t(3)) == 2);
+		assert(Settings::Get("d", std::uint16_t(4)) == 3);
+		assert(Settings::Get("e", std::int32_t(5)) == 4);
+		assert(Settings::Get("f", std::uint32_t(6)) == 5);
+		assert(Settings::Get("g", float(7.7)) == 6.6f);
+		assert(Settings::Get("h", std::int64_t(8)) == 7);
+		assert(Settings::Get("i", std::uint64_t(9)) == 8);
+		assert(Settings::Get("j", 10.1) == 9.9);
+	}
+
+	template <class T1, class T2>
+	void testSerialization2() {
+		assert(Settings::Set("a", std::int8_t(0)));
+		assert(Settings::Set("b", std::uint8_t(1)));
+		assert(Settings::Set("c", std::int16_t(2)));
+		assert(Settings::Set("d", std::uint16_t(3)));
+		assert(Settings::Set("e", std::int32_t(4)));
+		assert(Settings::Set("f", std::uint32_t(5)));
+		assert(Settings::Set("g", float(6.6)));
+		assert(Settings::Set("h", std::int64_t(7)));
+		assert(Settings::Set("i", std::uint64_t(8)));
+		assert(Settings::Set("j", 9.9));
+
+		Settings::Write<T2>("b.ini");
+		Settings::Read<T1>("b.ini");
 
 		assert(Settings::Get("a", std::int8_t(1)) == 0);
 		assert(Settings::Get("b", std::uint8_t(2)) == 1);
@@ -181,48 +310,67 @@ namespace internal{
 	//Runs all tests
 	void runAllTests() {
 		internal::testGetProperInputs();
-		internal::testSerialization();
-		internal::testGetNonDefinedSettings();
+		internal::testGetProperInputs2();
+		internal::testGetUndefinedSettings();
+		internal::testGetUndefinedSettings2();
+
+		internal::testSerialization<boost::archive::binary_iarchive, boost::archive::binary_oarchive>();
+		internal::testSerialization2<boost::archive::binary_iarchive, boost::archive::binary_oarchive>();
+
+		internal::testSerialization<boost::archive::xml_iarchive, boost::archive::xml_oarchive>();
+		internal::testSerialization2<boost::archive::xml_iarchive, boost::archive::xml_oarchive>();
+
+		internal::testSerialization<boost::archive::text_iarchive, boost::archive::text_oarchive>();
+		internal::testSerialization2<boost::archive::text_iarchive, boost::archive::text_oarchive>();
 
 		std::cout << "All tests have successfully completed." << std::endl;
 	}
-
 }
 
-//Public API
+//Publically Accessible Methods
 namespace Settings {
 
-
-
 	template<typename T>
-	T Get(std::string propertyName, T defaultValue) {
+	T Get(const std::string & propertyName, T defaultValue) {
 		return tableWrapper.Get<T>(propertyName, defaultValue);
 	}
 
 	template<typename T>
-	bool Set(std::string propertyName, T defaultValue) {
-		return tableWrapper.Set<T>(propertyName, defaultValue);
+	bool Get(const std::string & propertyName, T defaultValue, T* propertyValue) {
+		if (propertyValue == nullptr) {
+			std::cerr << "Error: a null pointer was passed as 'propertyValue'" << std::endl;
+			exit(1); return false; //Which one should I do??
+		}
+		return tableWrapper.Get<T>(propertyName, defaultValue, propertyValue);
 	}
 
+	template<typename T>
+	bool Set(const std::string & propertyName, T propertyValue) {
+		return tableWrapper.Set<T>(propertyName, propertyValue);
+	}
+
+	template <class T>
 	void Write() {
 		if (filename == "") {
 			std::cerr <<
 				"Warning: Attempted to write settings to file with Settings::Write() without first" 
-				"making a call to Settings::Read(const std::string &).  Use Settings::SetFilename(std::string)" 
+				"making a call to Settings::Read(const std::string &).  Use Settings::SetFilename(const std::string &)" 
 				"or Settings::Write(const std::string &) to specify where the file should be saved" << std::endl;
 			return;
 		}
 
-		tableWrapper.Write();
+		tableWrapper.Write<T>();
 	}
 
+	template <class T>
 	void Write(const std::string & _filename) {
 		filename = _filename;
-		tableWrapper.Write();
+		tableWrapper.Write<T>();
 	}
 
+	template <class T>
 	void Read(const std::string & _filename) {
-		tableWrapper.Read(_filename);
+		tableWrapper.Read<T>(_filename);
 	}
 
 	void SetFilename(const std::string & _filename) {
@@ -232,7 +380,7 @@ namespace Settings {
 
 
 
-int main() {
-	internal::runAllTests();
-	return 0;
-}
+//int main() {
+//	internal::runAllTests();
+//	return 0;
+//}
--- Soul Engine/Source Files/Utility/Settings.h
+++ Soul Engine/Source Files/Utility/Settings.h
@@ -1,16 +1,19 @@
 #pragma once
 
-//#include <boost/property_tree/ptree.hpp>
+//Boost library inclues
 #include <boost/variant.hpp>
 #include <boost/serialization/unordered_map.hpp>
 #include <boost/serialization/string.hpp>
+#include <boost/serialization/variant.hpp>
 #include <boost/archive/xml_oarchive.hpp>
 #include <boost/archive/xml_iarchive.hpp>
-#include <boost/serialization/variant.hpp>
 #include <boost/archive/text_oarchive.hpp>
 #include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/binary_oarchive.hpp>
+#include <boost/archive/binary_iarchive.hpp>
 #include <boost/serialization/nvp.hpp>
-#include <boost/static_assert.hpp>
+
+//Standard Library includes
 #include <string>
 #include <unordered_map>
 #include <cstdint>
@@ -19,7 +22,21 @@
 #include <utility>
 #include <type_traits>
 
+
 namespace internal {
+
+	//Type definitions for data structures used to store settings of
+	//different sizes
+
+
+	//If in the future even more performance is needed, you can replace this typedef
+	//with something of the following format.  You will have to write a boost serialize method.
+	
+	/*struct onebytetypes {
+		union { unsigned char, char } data;
+		unsigned char which;
+	};*/
+
 	typedef boost::variant<std::int8_t, std::uint8_t> OneByteTypes;
 	typedef boost::variant<std::int16_t, std::uint16_t> TwoByteTypes;
 	typedef boost::variant<std::int32_t, std::uint32_t, float> FourByteTypes;
@@ -56,6 +73,7 @@ namespace internal {
 		//These allow data to be set only when the type of the property value
 		//and unordered map match
 
+
 		//Setting OneByteTypes
 
 		bool set(OBM & dict, const std::string & propertyName, std::int8_t propertyValue) {
@@ -73,6 +91,7 @@ namespace internal {
 			return false;
 		}
 
+
 		//Setting TwoByteTypes
 
 		bool set(TBM & dict, const std::string & propertyName, std::int16_t propertyValue) {
@@ -89,6 +108,7 @@ namespace internal {
 		bool set(TBM & dict, const std::string & propertyName, T propertyValue) {
 			return false;
 		}
+
 
 		//Setting FourByteTypes
 
@@ -111,6 +131,7 @@ namespace internal {
 		bool set(FBM & dict, const std::string & propertyName, T propertyValue) {
 			return false;
 		}
+
 
 		//Setting EightByteTypes
 
@@ -137,11 +158,7 @@ namespace internal {
 
 	public:
 		//Default Constructor
-		//Table() : obt(), tbt(), fbt(), ebt(), filename() {}
 		Table() = default;
-
-		//Read file static constructor
-		//static Table ReadTable(const std::string & filename);
 
 		//Retrieves a value at the specified placeholder. Cannot accept user defined types, only system types
 		//If a template T is not explicitly provided, defaultvalue must be the same exact type as the value you 
@@ -172,24 +189,34 @@ namespace internal {
 	private:
 		Table* table;
 	public:
+
+		//Constructor and Destructor
 		TableWrapper() : table(new Table()) {}
 		~TableWrapper() { delete table; }
+
+		//Read settings from file
+		//T is a boost text, xml, or binary iarchive.  Wide character archives are not supported.
+		template <class T>
 		void Read(const std::string & _filename);
+
+		//Write settings to file
+		//T is a boost text, xml, or binary oarchive.  Wide character archives are not supported.
+		template <class T>
 		void Write();
 
-		//See header for Settings::Table::Get(const std::string&,T) for more info
+		//See Settings::Table::Get(const std::string&,T) for more info
 		template <typename T>
 		T Get(const std::string & propertyName, T defaultValue) {
 			return this->table->Get(propertyName, defaultValue);
 		}
 
-		//See header for Settings::Table::Get(const std::string&,T,T*) for more info
+		//See Settings::Table::Get(const std::string&,T,T*) for more info
 		template <typename T>
 		bool Get(const std::string & propertyName, T defaultValue, T* propertyValue) {
 			return this->table->Get(propertyName, defaultValue, propertyValue);
 		}
 
-		//See header for Settings::Table::Set(const std::string&,T) for more info
+		//See Settings::Table::Set(const std::string&,T) for more info
 		template <typename T>
 		bool Set(const std::string & propertyName, T propertyValue) {
 			return this->table->Set(propertyName, propertyValue);
@@ -198,20 +225,47 @@ namespace internal {
 	};
 }
 
-//Public API
+//Publically Accessible Methods
 namespace Settings {
 
+	//Retrieves a value at the specified placeholder. Cannot accept user defined types, only system types
+	//If a template T is not explicitly provided, defaultvalue must be the same exact type as the value you 
+	//are retrieving.  For example, if you are retrieving a float, default value must be 1.0f and not 1.
 	template<typename T>
-	T Get(std::string propertyName, T defaultValue);
+	T Get(const std::string & propertyName, T defaultValue);
 
+	//Retrieves a value at the specified placeholder. Cannot accept user defined types, only system types
+	//If a template T is not explicitly provided, defaultvalue must be the same exact type as the value you 
+	//are retrieving.  For example, if you are retrieving a float, default value must be 1.0f and not 1.
+	//Retrieved value is assigned to the given pointer.
+	//returns true if value was retrieved, and false if defaultvalue was assigned to pointer.
 	template<typename T>
-	bool Set(std::string propertyName, T defaultValue);
+	bool Get(const std::string & propertyName, T defaultValue, T* propertyValue);
 
+	//Set value, overriding any point
+	//Returns true if data was successfully set, else false
+	template<typename T>
+	bool Set(const std::string & propertyName, T propertyValue);
+
+	//Writes settings to a file already specified by SetFilename(const std::string &) or
+	//Read(const std::string & _filename).  Does nothing if a filename has not already been set.
+	//This is included to ensure compatability with previously written code before 
+	//Write(const std::string & _filename) was available.
+	//T is a boost text, XML, or binary oarchive.  Wide character archives are not supported.
+	template <class T = boost::archive::text_oarchive>
 	void Write();
 
+	//Writes settings to the file _filename
+	//T is a boost text, XML, or binary oarchive.  Wide character archives are not supported.
+	template <class T = boost::archive::text_oarchive>
 	void Write(const std::string & _filename);
 
+	//Deletes all currently stored settings and reads new ones from _filename.
+	//T is a boost text, XML, or binary iarchive.  Wide character archives are not supported.
+	template <class T = boost::archive::text_iarchive>
 	void Read(const std::string & _filename);
 
+	//Sets filename to _filename to allow Write() to be called for code using
+	//Write instead of Write(const std::string & _filename).
 	void SetFilename(const std::string & _filename);
 }
