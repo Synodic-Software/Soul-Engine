1e578d2a277aa1b3b3d404225eb7a93555edcfce
Behemyth <asher.norland@gmail.com>
2017-03-14 00:45:14 -0400

    Added GPU buffer framework

---
+++ Source Files/GPGPU/OpenCL/OpenCLBuffer.cpp
@@ -0,0 +1,11 @@
+#include "OpenCLBuffer.h"
+
+OpenCLBuffer::OpenCLBuffer() {
+
+
+}
+
+OpenCLBuffer::~OpenCLBuffer() {
+
+
+}
---
+++ Source Files/GPGPU/OpenCL/OpenCLBuffer.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "GPGPU\GPUBuffer.h"
+
+class OpenCLBuffer :public GPUBuffer {
+
+public:
+	OpenCLBuffer();
+	~OpenCLBuffer();
+
+protected:
+
+private:
+
+};
