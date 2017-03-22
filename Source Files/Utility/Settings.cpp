f1bdfdd7915493a7aff17d4f0690e605e355940d
Behemyth <asher.norland@gmail.com>
2017-03-21 23:07:34 -0400

    error fix

--- Source Files/Utility/Settings.cpp
+++ Source Files/Utility/Settings.cpp
@@ -1,4 +1,5 @@
 #include "Settings.h"
+#include <boost/filesystem.hpp>
 
 //Will try to minimize size of boost variants.  May have a negative impact on performance.
 //For more info see the boost::variant documentation
@@ -13,7 +14,7 @@ NOTE: See header file "Settings.h" for descriptions of all methods.
 namespace Settings {
 
 	//"Under the hood" functions to handle settings.  Should not be used externally.
-	namespace detail{
+	namespace detail {
 
 		std::string filename;
 		TableWrapper tableWrapper;
@@ -36,15 +37,17 @@ namespace Settings {
 	}
 
 	void Read(const std::string & _filename, FileType type) {
+		if (boost::filesystem::exists(_filename)) {
 
-		if (type == TEXT) {
-			detail::tableWrapper.Read<boost::archive::text_iarchive>(_filename);
-		}
-		else if (type == XML) {
-			detail::tableWrapper.Read<boost::archive::xml_iarchive>(_filename);
-		}
-		else {
-			detail::tableWrapper.Read<boost::archive::binary_iarchive>(_filename);
+			if (type == TEXT) {
+				detail::tableWrapper.Read<boost::archive::text_iarchive>(_filename);
+			}
+			else if (type == XML) {
+				detail::tableWrapper.Read<boost::archive::xml_iarchive>(_filename);
+			}
+			else {
+				detail::tableWrapper.Read<boost::archive::binary_iarchive>(_filename);
+			}
 		}
 	}
 
