aeaa830f6a0451149d2fdbed3d07cfd81b776396
Behemyth <asher.norland@gmail.com>
2017-04-07 01:37:10 -0400

    small refactoring

---
+++ Source Files/Algorithms/Morton Code/CUDA/MortonCode.cuh
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "Metrics.h"
+#include "Bounding Volume Heirarchy\BVH.h"
+
+namespace MortonCode{
+
+	__global__ void Compute(const uint n, uint64* mortonCodes, Face** faceList, Object** objectList, const BoundingBox box);
+
+}
