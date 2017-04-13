286af276a62b2a7c6bf481adb3c2ba394a97c958
Behemyth <asher.norland@gmail.com>
2017-04-13 01:58:06 -0400

    sky change

--- Source Files/Engine Core/Scene/CUDA/Sky.cu
+++ Source Files/Engine Core/Scene/CUDA/Sky.cu
@@ -16,5 +16,6 @@ __device__ glm::vec3 Sky::ExtractColour(const glm::vec3& direction){
 	float4 col = tex2D<float4>(image->texObj, theta, gamma );*/
 
 
-	return glm::mix(glm::vec3(255.0f / 255.0f, 255.0f / 255.0f, 255.0f / 255.0f),glm::vec3(135 / 255.0f, 206 / 255.0f, 235 / 255.0f), direction.y/2.0f+0.5f);
+	float normalized = direction.y / 2.0f + 0.5f;
+	return glm::mix(glm::vec3(255.0f / 255.0f, 255.0f / 255.0f, 255.0f / 255.0f),glm::vec3(135 / 255.0f, 200 / 255.0f, 240 / 255.0f),normalized/2.0f+0.5f);
 }
