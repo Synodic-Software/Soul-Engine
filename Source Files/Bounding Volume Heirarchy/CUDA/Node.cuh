862c168f8d9aee9443f5e74f53ffab3031d29031
Behemyth <asher.norland@gmail.com>
2017-04-14 02:05:11 -0400

    added overloads

--- Source Files/Bounding Volume Heirarchy/CUDA/Node.cuh
+++ Source Files/Bounding Volume Heirarchy/CUDA/Node.cuh
@@ -7,8 +7,8 @@
 class Node
 {
 public:
-	Node();
-	~Node();
+	__device__ __host__ Node();
+	__device__ __host__ ~Node();
 
 
 	Node* childLeft;
@@ -22,7 +22,26 @@ public:
 	uint rangeRight;
 	uint rangeLeft;
 
+	__host__ __device__ bool operator==(const Node& other) const {
+		return
+			childLeft == other.childLeft &&
+			childRight == other.childRight &&
+			rangeRight == other.rangeRight &&
+			rangeLeft == other.rangeLeft;
+	}
 
+	__host__ __device__ Node& operator=(Node arg)
+	{
+		this->childLeft = arg.childLeft;
+		this->childRight = arg.childRight;
+		this->rangeRight = arg.rangeRight;
+		this->rangeLeft = arg.rangeLeft;
+		this->atomic = arg.atomic;
+		this->faceID = arg.faceID;
+		this->box = arg.box;
+
+		return *this;
+	}
 	//__host__ __device__ void TransformRay(Ray&);
 
 private:
