2b1621e658f8ec2ec26aca441e0efdb9120c378d
Behemyth <asher.norland@gmail.com>
2017-05-04 16:59:36 -0400

    Revert "fixes"
    This reverts commit e593397d5799c47e916bb90230b1f3baa5a1de5e.

--- Source Files/Algorithms/Morton Code/CUDA/MortonCode.cu
+++ Source Files/Algorithms/Morton Code/CUDA/MortonCode.cu
@@ -5,11 +5,12 @@
 #include <inttypes.h>
 #include <stdint.h>
 
+//morton codes precomputed
 
 __inline__ __host__ __device__ uint64 Split(unsigned int a) {
 	uint64 x = a;
-	x = (x | x << 32) & 0x1f00000000ffff;
-	x = (x | x << 16) & 0x1f0000ff0000ff;
+	x = (x | x << 32) & 0x1f00000000ffff;  
+	x = (x | x << 16) & 0x1f0000ff0000ff; 
 	x = (x | x << 8) & 0x100f00f00f00f00f;
 	x = (x | x << 4) & 0x10c30c30c30c30c3;
 	x = (x | x << 2) & 0x1249249249249249;
@@ -18,7 +19,7 @@ __inline__ __host__ __device__ uint64 Split(unsigned int a) {
 
 __host__ __device__ uint64 MortonCode::CalculateMorton(const glm::vec3& data) {
 
-	uint max = 2097151;
+	uint max= 2097151;
 	uint x = uint(data.x*max);
 	uint y = uint(data.y*max);
 	uint z = uint(data.z*max);
@@ -29,31 +30,7 @@ __host__ __device__ uint64 MortonCode::CalculateMorton(const glm::vec3& data) {
 }
 
 
-// HELPER METHOD for Magic bits decoding
-__inline__ __host__ __device__ uint Unsplit(const uint64 m) {
-	uint64 x = m & 0x1249249249249249;
-	x = (x ^ (x >> 2)) & 0x10c30c30c30c30c3;
-	x = (x ^ (x >> 4)) & 0x100f00f00f00f00f;
-	x = (x ^ (x >> 8)) & 0x1f0000ff0000ff;
-	x = (x ^ (x >> 16)) & 0x1f00000000ffff;
-	x = (x ^ (x >> 32)) & 0x1fffff;
-	return x;
-}
-
-
-__host__ __device__ glm::vec3 DecodeMorton(uint64 m) {
-
-	uint x = Unsplit(m);
-	uint y = Unsplit(m >> 1);
-	uint z = Unsplit(m >> 2);
-
-	float max = 2097151;
-	glm::vec3 data = glm::vec3(x / max, y / max, z / max);
-
-	return data;
-}
-
-__global__ void MortonCode::Compute(const uint n, Face* faces, Vertex* vertices) {
+__global__ void MortonCode::Compute(const uint n,Face* faces, Vertex* vertices) {
 
 	uint index = getGlobalIdx_1D_1D();
 
--- Source Files/Engine Core/Scene/CUDA/SceneNode.cu
+++ Source Files/Engine Core/Scene/CUDA/SceneNode.cu
@@ -4,10 +4,11 @@
 
 SceneNode::SceneNode(glm::mat4 tr) {
 
-	uint64 mortonMin = MortonCode::CalculateMorton((tr*glm::vec4(-1.0f, -1.0f, -1.0f, 1.0f) + glm::vec4(1.0f)) / 2.0f);
-	uint64 mortonMax = MortonCode::CalculateMorton((tr*glm::vec4(1.0f, 1.0f, 1.0f, 1.0f) + glm::vec4(1.0f)) / 2.0f);
+	uint64 mortonMin = MortonCode::CalculateMorton(tr*glm::vec4(-1.0f, -1.0f, -1.0f, 1.0f));
+	uint64 mortonMax = MortonCode::CalculateMorton(tr*glm::vec4(1.0f, 1.0f, 1.0f, 1.0f));
 
-	bitsUsed = 64 - __lzcnt64(mortonMin^mortonMax);
-	morton = mortonMin >> bitsUsed;
+	uint64 leadingZeros = __lzcnt64(mortonMin);
+	uint64 lowestBit = (64 - leadingZeros) - (__lzcnt64(mortonMin^mortonMax) - leadingZeros);
+	morton = mortonMin >> lowestBit;
 
 }
