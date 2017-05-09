00efce846c60710625ee2a7e30383334b02df857
Behemyth <asher.norland@gmail.com>
2017-05-09 14:41:47 -0500

    small changes

--- Source Files/Data Structures/Radix Tree/RTNode.h
+++ Source Files/Data Structures/Radix Tree/RTNode.h
@@ -1,11 +1,16 @@
 #pragma once
+#include <boost/dynamic_bitset.hpp>
+#include "Metrics.h"
+#include <vector>
 
 class RTNode
 {
 public:
-	RTNode();
+	RTNode(boost::dynamic_bitset<>&);
+	void AddSequence(boost::dynamic_bitset<>&);
 
 private:
-
+	boost::dynamic_bitset<> bits;
+	std::vector<RTNode> children;
 };
 
--- Source Files/Data Structures/Radix Tree/RadixTree.h
+++ Source Files/Data Structures/Radix Tree/RadixTree.h
@@ -1,11 +1,31 @@
 #pragma once
 
+#include "RTNode.h"
+#include <vector>
+#include "Metrics.h"
+#include <boost/multiprecision/gmp.hpp>
+
+namespace boost {
+	namespace multiprecision {
+		class gmp_int;
+		typedef number<gmp_int> mpInt;
+	}
+}
+
+typedef boost::multiprecision::mpInt ArbitraryInt;
+
+
 class RadixTree
 {
 public:
-	RadixTree();
+
+
+	RadixTree(uint);
+
+	void AddSequence(boost::dynamic_bitset<>&);
 
 private:
-
+	RTNode root;
+	uint bitsToSplit;
 };
 
