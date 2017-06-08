ed06c1b0172db719ad81ac79991b982dcd1ae0df
Behemyth <asher.norland@gmail.com>
2017-06-08 00:50:16 -0500

    Dynamic samples

--- Source Files/Ray Engine/RayEngine.cpp
+++ Source Files/Ray Engine/RayEngine.cpp
@@ -1,12 +1,35 @@
 #include "RayEngine.h"
 #include "CUDA/RayEngine.cuh"
 #include "Utility\CUDA\CUDAHelper.cuh"
+#include "Utility\Timer.h"
 
 
 static std::list<RayJob*> jobList;
 
+//timer
+static Timer timer;
+
+void UpdateJobs(double in, double target, std::list<RayJob*>& jobs) {
+
+	double ratio = target / in;
+
+	for (auto& job : jobs) {
+		if (job->canChange) {
+			job->samples *= ratio;
+		}
+	}
+
+}
+
 void RayEngine::Process(const Scene* scene){
+
+	//start the timer once acctual dat movement and calculation starts
+	timer.Reset();
+
 	ProcessJobs(jobList, scene);
+
+	UpdateJobs(timer.Elapsed(), 14.0, jobList);
+
 }
 
 RayJob* RayEngine::AddJob(rayType whatToGet, uint rayAmount, bool canChange,
