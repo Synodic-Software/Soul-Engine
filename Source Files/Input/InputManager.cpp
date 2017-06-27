e859fb8ce54a05244b56641250a717fcb776095d
Behemyth <asher.norland@gmail.com>
2017-06-27 15:23:53 -0500

    Added callbacks back in

--- Source Files/Input/InputManager.cpp
+++ Source Files/Input/InputManager.cpp
@@ -1,3 +1,45 @@
 #include "InputManager.h"
+#include "Multithreading/Scheduler.h"
+
+namespace InputManager {
+
+	namespace detail {
+
+		void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+			
+		}
+
+		void characterCallback(GLFWwindow* window, unsigned int codepoint) {
+			
+		}
+
+		void cursorCallback(GLFWwindow* window, double xpos, double ypos) {
+			
+		}
+
+		void scrollCallback(GLFWwindow* window, double xoffset, double yoffset) {
+			
+		}
+
+		void buttonCallback(GLFWwindow* window, int button, int action, int mods) {
+			
+		}
+
+	}
+
+	void AttachWindow(GLFWwindow* window) {
+
+		Scheduler::AddTask(LAUNCH_IMMEDIATE, FIBER_HIGH, true, [=]() {
+			glfwSetKeyCallback(window,detail::keyCallback);
+			glfwSetCharCallback(window, detail::characterCallback);
+			glfwSetCursorPosCallback(window, detail::cursorCallback);
+			glfwSetScrollCallback(window, detail::scrollCallback);
+			glfwSetMouseButtonCallback(window, detail::buttonCallback);
+		});
+
+		Scheduler::Block();
+
+	}
 
 
+}
--- Source Files/Input/InputManager.h
+++ Source Files/Input/InputManager.h
@@ -6,10 +6,61 @@
 /* . */
 namespace InputManager {
 
-/* . */
+	/* . */
 	namespace detail {
 
+		/*
+		 *    Callback, called when the key.
+		 *    @param [in,out]	window  	If non-null, the window.
+		 *    @param 		 	key			The key.
+		 *    @param 		 	scancode	The scancode.
+		 *    @param 		 	action  	The action.
+		 *    @param 		 	mods		The mods.
+		 */
+
+		void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
+
+		/*
+		 *    Callback, called when the character.
+		 *    @param [in,out]	window   	If non-null, the window.
+		 *    @param 		 	codepoint	The codepoint.
+		 */
+
+		void characterCallback(GLFWwindow* window, unsigned int codepoint);
+
+		/*
+		 *    Callback, called when the cursor.
+		 *    @param [in,out]	window	If non-null, the window.
+		 *    @param 		 	xpos  	The xpos.
+		 *    @param 		 	ypos  	The ypos.
+		 */
+
+		void cursorCallback(GLFWwindow* window, double xpos, double ypos);
+
+		/*
+		 *    Callback, called when the scroll.
+		 *    @param [in,out]	window 	If non-null, the window.
+		 *    @param 		 	xoffset	The xoffset.
+		 *    @param 		 	yoffset	The yoffset.
+		 */
+
+		void scrollCallback(GLFWwindow* window, double xoffset, double yoffset);
+
+		/*
+		 *    Callback, called when the button.
+		 *    @param [in,out]	window	If non-null, the window.
+		 *    @param 		 	button	The button.
+		 *    @param 		 	action	The action.
+		 *    @param 		 	mods  	The mods.
+		 */
+
+		void buttonCallback(GLFWwindow* window, int button, int action, int mods);
 	}
 
+	/*
+	 *    Attach window.
+	 *    @param [in,out]	window	If non-null, the window.
+	 */
 
+	void AttachWindow(GLFWwindow* window);
 };
