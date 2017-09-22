b1945ab57f5ce793d6c03f3f525ec7e35f73705e
gartea <gartea@rpi.edu>
2017-09-22 19:04:13 -0400

    #85 added schedular tests testing 1 tasks and 400 tasks

--- Test/Multithreading test/unittest1.cpp
+++ Test/Multithreading test/unittest1.cpp
@@ -16,7 +16,7 @@ namespace Multithreadingtest
 	{
 	public:
 		
-		TEST_METHOD(fiber_spawning)
+		TEST_METHOD(basic_fiber_spawning)
 		{
 			//test intitializing the scheduler
 			Scheduler::Initialize();
@@ -25,6 +25,39 @@ namespace Multithreadingtest
 			Scheduler::AddTask(LAUNCH_IMMEDIATE, FIBER_HIGH, false, []() {
 				std::cout << "hi" << std::endl;
 			});
+
+			Assert::IsTrue(Scheduler::Running());
+
+			Scheduler::Block();
+
+			Scheduler::Terminate();
+
+			Assert::IsTrue(true);
+		}
+		TEST_METHOD(Multi_fiber_spawning)
+		{
+			//test intitializing the scheduler
+			Scheduler::Initialize();
+
+			for (UINT i = 0; i < 100; i++)
+			{
+				Scheduler::AddTask(LAUNCH_IMMEDIATE, FIBER_HIGH, false, []() {
+					mult(1,1);
+				});
+				Scheduler::AddTask(LAUNCH_CONTINUE, FIBER_HIGH, false, []() {
+					mult(1, 1);
+				});
+				Scheduler::AddTask(LAUNCH_IMMEDIATE, FIBER_LOW, false, []() {
+					mult(1, 1);
+				});
+				Scheduler::AddTask(LAUNCH_CONTINUE, FIBER_LOW, false, []() {
+					mult(1, 1);
+				});
+
+			}
+			
+
+			Assert::IsTrue(Scheduler::Running());
 
 			Scheduler::Block();
 
