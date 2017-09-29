1389e0b35b44c5b04c9993e3599d94e3e66acefa
Behemyth <asher.norland@gmail.com>
2017-09-28 23:55:44 -0400

    Reimplemented pos processing function

--- Source Files/Algorithms/Filters/CUDA/Filter.cu
+++ Source Files/Algorithms/Filters/CUDA/Filter.cu
@@ -1,16 +1,26 @@
 #include "Filter.cuh"
 
-__global__ void GPUHermiteBicubic(glm::vec4* data, glm::uvec2 originalSize, glm::uvec2 desiredSize) {
+__global__ void GPUHermiteBicubic(uint n, glm::vec4* data, glm::uvec2 originalSize, glm::uvec2 desiredSize) {
+	uint index = getGlobalIdx_1D_1D();
+
+	if (index >= n) {
+		return;
+	}
 
 }
 
-__host__ void CUDAFilter::HermiteBicubic(GPUBuffer<glm::vec4>& data, glm::uvec2 originalSize, glm::uvec2 desiredSize) {
+namespace CUDAFilter {
 
-	uint count = desiredSize.x*desiredSize.y;
+	__host__ void HermiteBicubic(glm::vec4* data, glm::uvec2& originalSize, glm::uvec2& desiredSize) {
 
-	uint blockSize = 64;
-	uint blockCount = (count + blockSize - 1) / blockSize;
+		uint count = desiredSize.x*desiredSize.y;
 
-	GPUHermiteBicubic << < blockCount, blockSize >> > (data, originalSize, desiredSize);
+		uint blockSize = 64;
+		uint blockCount = (count + blockSize - 1) / blockSize;
+
+		GPUHermiteBicubic << < blockCount, blockSize >> > (count, data, originalSize, desiredSize);
+		CudaCheck(cudaPeekAtLastError());
+		CudaCheck(cudaDeviceSynchronize());
+	}
 
 }
