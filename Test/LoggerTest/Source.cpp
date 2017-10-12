f1ac661d8450dbe1b15696693ebe5970dfb245f9
sonj2 <sonj2@rpi.edu>
2017-10-11 23:36:28 -0400

    Add boost libraries

--- Test/LoggerTest/Source.cpp
+++ Test/LoggerTest/Source.cpp
@@ -8,7 +8,7 @@ int main()
 	Logger::WriteFile();
 	std::ifstream file("Engine.log");
 	std::getline(file, line);
-	//assert(line.compare("File: Engine.log Line: 7 | ") == 0);
+	assert(line.compare("File: Engine.log Line: 7 | ") == 0);
 	file.close();
 	return 0;
 }
