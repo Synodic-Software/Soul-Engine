bbce117e4da8f4ddfa39685ed1f1add69c2882a6
sonj2 <sonj2@rpi.edu>
2017-10-31 15:26:06 -0400

    Time print on writefile()
    current date time prints and tested on logger test file, formatting is default

--- Test/LoggerTest/Source.cpp
+++ Test/LoggerTest/Source.cpp
@@ -4,9 +4,14 @@
 
 int main()
 {
+	namespace pt = boost::posix_time;
 	std::string line;
+	std::string time;
+	time = pt::to_iso_string(pt::second_clock::local_time());
 	Logger::WriteFile();
 	std::ifstream file("Engine.log");
+	std::getline(file, line);
+	assert(line.compare(time) == 0);
 	std::getline(file, line);
 	assert(line.compare("File: Engine.log Line: 7 | ") == 0);
 	file.close();
