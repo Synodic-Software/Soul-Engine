37435eb757f314ce9c47a67b6136cfa6e862232e
Louis Silvestro <silvel2@rpi.edu>
2017-12-02 15:50:58 -0500

    Start TaggedHeap implementation

--- Source Files/Memory/TaggedHeap.h
+++ Source Files/Memory/TaggedHeap.h
@@ -1,21 +1,19 @@
 #pragma once
 #include <map>
-
+#include <list>
 /* This class allows for used memory to be tagged on a per-module basis*/
 class TaggedHeap {
 	private:
 		/*Private variables*/
 		size_t numAllocs;
 		void* heap;
-		std::map<string, void*> tags; // associate module tag with its share of memory
+		std::map<std::string, std::list<std::pair<void*, size_t>>> tags; // associate module tag with its share of memory
 		size_t usedSpace;
-		
-		/*Private functions*/
-		void* addMem(std::string tag); //add additional memory to a tagged block
+
 	
 	public:
 		TaggedHeap(void*); // Constructor for creating TaggedHeap
-		void requestBlocks(std::string tag, size_t numBlocks, uint8_t align); // Request blocks from a specific tag
+		void requestBlocks(std::string tag, size_t size); // Request blocks from a specific tag
 		void clearTag(std::string tag); // Clear memory from a given tag
 		void clearAll(); // Clear the entire heap
 };
