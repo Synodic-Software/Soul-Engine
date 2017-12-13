da584f5bd566e996db31da97fb580120f8a5a870
Louis Silvestro <silvel2@rpi.edu>
2017-12-12 23:50:31 -0500

    Finish memory manager implementation

--- Source Files/Memory/TaggedAllocator/TaggedAllocator.h
+++ Source Files/Memory/TaggedAllocator/TaggedAllocator.h
@@ -1,21 +1,26 @@
 #pragma once
-#include "LinearAllocator.h"
+#include "..\Allocator.h"
 #include "TaggedHeap.h"
 #include <list>
-
+#include <mutex>
 /*Tagged Allocator is an allocator class that utilizes the tagged heap for per-module memory management.
   It uses a LinearAllocator under the hood to make the actual allocations, but obtains memory from the TaggedHeap.
   Memory allocation happens on a per-thread bases to make this allocator thread-safe*/
-class TaggedAllocator {
+class TaggedAllocator : public Allocator {
+	mutable std::mutex _mutex;
+
 	private:
 		// Private variables
-		TaggedHeap heap;
-		LinearAllocator allocator
-		std::map<long, std::list<std::pair<void*, size_t>>> blocks;
+		TaggedHeap* _heap;
+		std::string _tag;
+		size_t _blockSize;
+		size_t _numBlocks;
+		
 
 	public:
-		TaggedAllocator(TaggedHeap heap, std::string tag);
-		void* allocate(long threadId, size_t, uint8_t);
+		TaggedAllocator(TaggedHeap* heap, const std::string &tag, uint8_t align=4);
+		void* allocate(size_t size, uint8_t align=4);
 		void* deallocate(void* block);
-
+		void clear();
+		
 };
