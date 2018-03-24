7e167170d7a20c600921824199c4fffffb9dd033
falkom <falkom@rpi.edu>
2018-03-24 16:03:47 -0400

    Commit before reverting to previous commit.

---
+++ Source/Display/Window/GLFW/GLFWManager.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <vulkan\vulkan.hpp>
+#include <GLFW\glfw3.h>
+
+#include <Display\Window\WindowManager.h>
+
+class GLFWManager : public WindowManager
+{
+public:
+
+private:
+
+};
