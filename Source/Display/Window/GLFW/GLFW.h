95a6700abfeb5ece76f39b8b3d8482eef6be7c12
falkom <falkom@rpi.edu>
2018-03-24 15:33:59 -0400

    Abstracted Window class. Beginning to abstract WindowManager Class.
    Refactored and abstracted Window.h/.cpp into GLFW.h/.cpp.
    
    Starting Refactoring and abstraction process with WindowManager.h/.cpp for the newly created files GLFWManager.h/.cpp.

---
+++ Source/Display/Window/GLFW/GLFW.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <vulkan/vulkan.hpp>
+#include <GLFW/glfw3.h>
+
+#include "Display\Window\Window.h"
+
+class GLFW : public Window
+{
+public:
+
+	/*
+	*    Constructor.
+	*    @param 		 	parameter1	The first parameter.
+	*    @param 		 	parameter2	The second parameter.
+	*    @param 		 	x		  	An uint to process.
+	*    @param 		 	y		  	An uint to process.
+	*    @param 		 	width	  	The width.
+	*    @param 		 	height	  	The height.
+	*    @param [in,out]	parameter7	If non-null, the parameter 7.
+	*    @param [in,out]	parameter8	If non-null, the parameter 8.
+	*/
+	
+	GLFW(WindowType, const std::string&, uint x, uint y, uint width, uint height, GLFWmonitor*, GLFWwindow*);
+	
+	/* Destructor. */
+	~GLFW();
+
+	/* Draws this object. */
+	void Draw();
+
+private:
+	/* Type of the window */
+	WindowType windowType;
+	
+	/* Handle of the window */
+	GLFWwindow* windowHandle;
+};
--- Source/Display/Window/Window.cpp
+++ Source/Display/Window/Window.cpp
@@ -1,9 +1,4 @@
 #include "Window.h"
-#include "Utility/Logger.h"
-#include "Raster Engine/RasterManager.h"
-#include "Multithreading/Scheduler.h"
-#include "WindowManager.h"
-#include "Input/InputManager.h"
 
 /*
  *    Constructor.
@@ -17,108 +12,12 @@
  *    @param [in,out]	sharedContext	If non-null, context for the shared.
  */
 
-Window::Window(WindowType inWin, const std::string& inTitle, uint x, uint y, uint iwidth, uint iheight, GLFWmonitor* monitorIn, GLFWwindow* sharedContext)
+Window::Window(const std::string& inTitle, uint x, uint y, uint iwidth, uint iheight)
 {
-	windowType = inWin;
 	xPos = x;
 	yPos = y;
 	width = iwidth;
 	height = iheight;
 	title = inTitle;
-	windowHandle = nullptr;
 
-	glfwWindowHint(GLFW_SAMPLES, GLFW_DONT_CARE);
-	glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
-
-	const GLFWvidmode* mode = glfwGetVideoMode(monitorIn);
-
-	glfwWindowHint(GLFW_RED_BITS, mode->redBits);
-	glfwWindowHint(GLFW_GREEN_BITS, mode->greenBits);
-	glfwWindowHint(GLFW_BLUE_BITS, mode->blueBits);
-	glfwWindowHint(GLFW_REFRESH_RATE, mode->refreshRate);
-
-	if (windowType == FULLSCREEN) {
-
-		glfwWindowHint(GLFW_RESIZABLE, false);
-		glfwWindowHint(GLFW_DECORATED, false);
-
-		windowHandle = glfwCreateWindow(width, height, title.c_str(), monitorIn, sharedContext);
-
-	}
-	else if (windowType == WINDOWED) {
-
-		glfwWindowHint(GLFW_RESIZABLE, true);
-
-		windowHandle = glfwCreateWindow(width, height, title.c_str(), nullptr, sharedContext);
-
-	}
-	else if (windowType == BORDERLESS) {
-
-		glfwWindowHint(GLFW_RESIZABLE, false);
-		glfwWindowHint(GLFW_DECORATED, false);
-
-		windowHandle = glfwCreateWindow(width, height, title.c_str(), nullptr, sharedContext);
-
-	}
-	else {
-		glfwWindowHint(GLFW_RESIZABLE, false);
-		glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
-
-		windowHandle = glfwCreateWindow(width, height, title.c_str(), nullptr, sharedContext);
-
-	}
-
-	if (windowHandle == nullptr)
-	{
-		S_LOG_FATAL("Could not Create GLFW Window");
-	}
-
-	//the backend is the new user
-
-	Window* thisWindow = this;
-
-	glfwSetInputMode(windowHandle, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
-	glfwSetWindowUserPointer(windowHandle, thisWindow);
-
-	//all window related callbacks
-	glfwSetWindowSizeCallback(windowHandle, [](GLFWwindow* w, int x, int y)
-	{
-		WindowManager::Instance().Resize(w, x, y);
-	});
-
-	glfwSetWindowPosCallback(windowHandle, [](GLFWwindow* w, int x, int y)
-	{
-		WindowManager::Instance().WindowPos(w, x, y);
-	});
-
-	glfwSetWindowRefreshCallback(windowHandle, [](GLFWwindow* w)
-	{
-		WindowManager::Instance().Refresh(w);
-	});
-
-	glfwSetWindowCloseCallback(windowHandle, [](GLFWwindow* w)
-	{
-		WindowManager::Instance().Close(w);
-	});
-
-	glfwShowWindow(windowHandle);
-
-
-	InputManager::AttachWindow(windowHandle);
-
-
-}
-
-/* Destructor. */
-Window::~Window()
-{
-	if (windowHandle) {
-		glfwDestroyWindow(windowHandle);
-	}
-}
-
-/* Draws this object. */
-void Window::Draw()
-{
-	layout->Draw();
 }
