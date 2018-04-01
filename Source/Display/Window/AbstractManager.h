76127afdb502e21b7dce4286ead2b83e26966b3b
falkom <falkom@rpi.edu>
2018-04-01 13:17:52 -0400

    Comments for AbstractManager.h

--- Source/Display/Window/AbstractManager.h
+++ Source/Display/Window/AbstractManager.h
@@ -10,38 +10,57 @@
 class AbstractManager
 {
 public:
+
+	/* 
+		Creates a static AbstractManager instance. 
+		Only one instance of the AbstractManager should exist at a time. 
+	*/
 	static AbstractManager& Instance() {
 		static AbstractManager instance;
 		return instance;
 	}
 
+	/* AbstractManager can be neither copied, nor assigned. */
 	AbstractManager(AbstractManager const&) = delete;
 	void operator=(AbstractManager const&) = delete;
 
+
+	/* Close operations. */
 	bool ShouldClose();
 	void SignalClose();
+	void Close(AbstractManager*);
 
+	/* Process. to create a window. */
 	AbstractWindow* CreateWindow(WindowType, const std::string&, int monitor, uint x, uint y, uint width, uint height);
 
+	/* Set the Window's Layout. */
 	void SetWindowLayout(AbstractManager*, Layout*);
 
+	/* Modifier operations. */
 	void Draw();
 	void Resize(AbstractWindow*, int, int);
 	void Refresh(AbstractManager*);
 	void WindowPos(AbstractManager*, int, int);
 
-	void Close(AbstractManager*);
-
 private:
 
+	/* Constructor. */
 	AbstractManager();
+	/* Destructor. */
 	~AbstractManager();
 
+	/* List of all windows handled by the AbstractManager. */
 	std::list<std::unique_ptr<AbstractWindow>> windows;
+	/* Pointer to the master--or 'main'--window. */
 	AbstractWindow* masterWindow = nullptr;
 
+	/* 
+		Monitor Count Variables. 
+		monitors is a void* for abstraction purposes.
+	*/
 	int monitorCount;
 	void** monitors;
 
+	/* Flag that keeps track of if the AbstractManager is running. */
 	bool runningFlag;
 };
