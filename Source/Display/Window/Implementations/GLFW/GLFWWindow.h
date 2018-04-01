cb547509d388efd47621ed81aa8570f399c87031
falkom <falkom@rpi.edu>
2018-04-01 17:21:00 -0400

    GLFWWindow.h

---
+++ Source/Display/Window/Implementations/GLFW/GLFWWindow.h
@@ -0,0 +1,14 @@
+#include "Display\Window\AbstractWindow.h"
+
+class GLFWWindow : public AbstractWindow 
+{
+	/* Constructor. */
+	GLFWWindow(WindowType, const std::string&, uint x, uint y, uint width, uint height, void*, void*);
+
+	/* Destructor. */
+	~GLFWWindow() = default;
+
+	/* Draws this object. */
+	void Draw();
+
+};
