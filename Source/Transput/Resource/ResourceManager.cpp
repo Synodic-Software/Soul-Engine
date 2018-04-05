7aee3ff8a5f4d622f39d9eda0d948e0ca4c60515
Behemyth <asher.norland@gmail.com>
2018-04-05 16:02:13 -0400

    Remove Crappy Temp Implementation

--- Source/Transput/Resource/ResourceManager.cpp
+++ Source/Transput/Resource/ResourceManager.cpp
@@ -1,36 +1,5 @@
-#pragma once
+#include "ResourceManager.h"
 
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <boost/filesystem.hpp>
-
-/*
-	Namespace to enclose all i/o operations.
-*/
-
-
-class FileManager {
-public:
-
-	/*
-			@param			filename_				name of file to operate on
-	*/
-	FileManager(const std::string & filename_) :filename(filename_) {}
-
-	/*
-			@effects				read from the file specified by gilename
-	*/
+ResourceManager::ResourceManager() {
 	
-	virtual void Read() = 0;
-
-	/*
-			@effects				write to the file specified by filename
-	*/
-	virtual void Write() = 0;
-
-protected:
-	std::string filename;
-
-
-};
+}
--- Source/Transput/Resource/ResourceManager.h
+++ Source/Transput/Resource/ResourceManager.h
@@ -1,23 +1,11 @@
 #pragma once
 
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <boost/filesystem.hpp>
-
-
-/*
-	Namespace to enclose all file i/o operations.
-*/
-namespace FileSystem {
-
-		/*
-				@effects				read from the file specified by filename
-		*/
-		void Read(std::string& filename);
+class ResourceManager {
 	
-		/*
-				@effects				write to the file specified by the filename
-		*/
-		void Write(std::string& filename);
-	};
+public:
+
+	ResourceManager();
+
+private:
+
+};
