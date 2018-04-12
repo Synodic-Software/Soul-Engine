7d711ecf9c09ecbf94b3e86a36f4fdcc5cc251b9
falkom <falkom@rpi.edu>
2018-04-12 19:01:50 -0400

    Remove OS-detect

--- OS-detect/windows32.h
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-#ifdef WINDOWS32_H
-void printOS(){
-	std::cout<<"OS: win32"<<std::endl;
-}
-#endif
--- OS-detect/windows64.h
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-#ifdef WINDOWS64_H
-void printOS(){
-	std::cout<<"OS: win64"<<std::endl;
-}
-#endif
