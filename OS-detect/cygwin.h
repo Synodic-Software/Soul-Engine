05d0f5da8fb1175992ae6f7b66fe606b81ebd795
falkom <falkom@rpi.edu>
2018-04-12 19:09:37 -0400

    Revert "OS-detect"
    This reverts commit 48e54b6661ede558666f79964e443e1272ccf1e4, reversing
    changes made to 9a4b5fcb3e4b0b440ddebfc0a5a7218a2cc26b73.

--- OS-detect/cygwin.h
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-#ifdef CYGWIN_H
-void printOS(){
-	std::cout<<"OS: cygwin"<<std::endl;
-}
-#endif
--- OS-detect/osx.h
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-#ifdef OSX_H
-void printOS(){
-	std::cout<<"OS: osx"<<std::endl;
-}
-#endif
