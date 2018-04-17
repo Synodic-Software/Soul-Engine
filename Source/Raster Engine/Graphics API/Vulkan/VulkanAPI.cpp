a220156c9956dbf1227260b6e48606d887009c2f
Letian Zhou <35975976+zhoul10@users.noreply.github.com>
2018-04-17 14:44:01 -0400

    create VK instance

--- Source/Raster Engine/Graphics API/Vulkan/VulkanAPI.cpp
+++ Source/Raster Engine/Graphics API/Vulkan/VulkanAPI.cpp
@@ -1,9 +1,47 @@
 #include "VulkanAPI.h"
 
 VulkanAPI::VulkanAPI() {
-	
+	InitInstance();
 }
 
 VulkanAPI::~VulkanAPI() {
+	DeInstance();
+}
+
+void VulkanAPI::InitInstance()
+{
+	VkApplicationInfo application_info{};
+	application_info.sType            = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+	application_info.apiVersion = VK_API_VERSION_1_0;
+	application_info.applicationVersion = VK_MAKE_VERSION(1,0, 0);
+	application_info.pApplicationName = "Soul Engine Test";
+	application_info.pEngineName = "Soul Engine";
+	application_info.engineVersion = VK_MAKE_VERSION(1, 0, 0);
 	
+		
+		
+	VkInstanceCreateInfo instance_create_info{};
+	instance_create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	instance_create_info.pApplicationInfo = &application_info;
+
+	uint32_t glfwExtensionCount = 0;
+	const char** glfwExtensions;
+
+	glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+	instance_create_info.enabledExtensionCount = glfwExtensionCount;
+	instance_create_info.ppEnabledExtensionNames = glfwExtensions;
+	instance_create_info.enabledLayerCount = 0;
+
+	auto err  =  vkCreateInstance(&instance_create_info,nullptr,&_instance);
+	if (VK_SUCCESS != err){
+		assert(0 && "Vulkan ERROR: Create instance failed.");
+	    std::exit(-1);
+	}
+}
+
+void VulkanAPI::DeInstance()
+{
+	vkDestroyInstance(_instance, nullptr);
+	_instance = nullptr;
 }
--- Source/Raster Engine/Graphics API/Vulkan/VulkanAPI.h
+++ Source/Raster Engine/Graphics API/Vulkan/VulkanAPI.h
@@ -2,11 +2,15 @@
 
 #include "Display\Window\Window.h"
 #include "Raster Engine/Graphics API/GraphicsAPI.h"
+#include <vulkan/vulkan.h>
 
 class VulkanAPI : public GraphicsAPI {
 public:
 
 	VulkanAPI();
 	~VulkanAPI();
-
-};
+private:
+	void InitInstance();
+	void DeInstance();
+	VkInstance       _instance = nullptr;
+ }; 
