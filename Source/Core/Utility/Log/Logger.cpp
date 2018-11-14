1902a0477062c59f123ad00c63e36ba1c430641c
emansyl <emmanuel.sylvester22@gmail.com>
2018-11-13 19:15:16 -0500

    Fixed indentation

--- Source/Core/Utility/Log/Logger.cpp
+++ Source/Core/Utility/Log/Logger.cpp
@@ -1,51 +1,51 @@
 #include "Logger.h"
 
-		std::string LogSeverityStrings[4]{
-			"TRACE",
-			"WARNING",
-			"ERROR",
-			"FATAL"
-		};
+std::string LogSeverityStrings[4]{
+	"TRACE",
+	"WARNING",
+	"ERROR",
+	"FATAL"
+};
 
-		/* The log mut */
-		std::mutex logMut;
-		/* The storage */
-		std::deque<Logger::LogI> storage;
+/* The log mut */
+std::mutex logMut;
+/* The storage */
+std::deque<Logger::LogI> storage;
 
-		/*
-		 *    Writes an information.
-		 *    @param [in,out]	oss 	The oss.
-		 *    @param 		 	file	The file.
-		 *    @param 		 	line	The line.
-		 */
+/*
+ *    Writes an information.
+ *    @param [in,out]	oss 	The oss.
+ *    @param 		 	file	The file.
+ *    @param 		 	line	The line.
+ */
 
-		void WriteInfo(std::ostream& oss, const char* file, int line) {		
-			oss << "File: TODO" << " Line: " << line << " | ";
-		}
-	
+void WriteInfo(std::ostream& oss, const char* file, int line) {
+	oss << "File: TODO" << " Line: " << line << " | ";
+}
 
-	/*
-	 *    Gets the get.
-	 *    @return	A std::string.
-	 */
 
-	std::string Get() {
-		logMut.lock();
-		if (storage.empty() > 0) {
+/*
+ *    Gets the get.
+ *    @return	A std::string.
+ */
 
-			Logger::LogI temp = storage.front();
-			storage.pop_front();
-			logMut.unlock();
+std::string Get() {
+	logMut.lock();
+	if (storage.empty() > 0) {
 
-			return ("[" + LogSeverityStrings[static_cast<int>(temp.severity)] + "] " + temp.msg+"/n");
-		}
-		else {
-			logMut.unlock();
-			return std::string();
-		}
+		Logger::LogI temp = storage.front();
+		storage.pop_front();
+		logMut.unlock();
+
+		return ("[" + LogSeverityStrings[static_cast<int>(temp.severity)] + "] " + temp.msg + "/n");
 	}
-
-	void WriteFile()
-	{
-		//TODO
+	else {
+		logMut.unlock();
+		return std::string();
 	}
+}
+
+void WriteFile()
+{
+	//TODO
+}
--- Source/Core/Utility/Log/Logger.h
+++ Source/Core/Utility/Log/Logger.h
@@ -19,92 +19,92 @@ class Logger {
 
 	//User: Do Not Touch
 
-public: 
+public:
 
-		template <typename T>
+	template <typename T>
 
-		/*
-		 *    Logs a help.
-		 *    @param [in,out]	o	A std::ostream to process.
-		 *    @param 		 	t	A T to process.
-		 */
+	/*
+	 *    Logs a help.
+	 *    @param [in,out]	o	A std::ostream to process.
+	 *    @param 		 	t	A T to process.
+	 */
 
-		void LogHelp(std::ostream& o, T t)
-		{
-			o << t;
-		}
+	void LogHelp(std::ostream& o, T t)
+	{
+		o << t;
+	}
 
-		template<typename T, typename... Args>
+	template<typename T, typename... Args>
 
-		/*
-		 *    Logs a help.
-		 *    @param [in,out]	o   	A std::ostream to process.
-		 *    @param 		 	t   	A T to process.
-		 *    @param 		 	args	Variable arguments providing the arguments.
-		 */
+	/*
+	 *    Logs a help.
+	 *    @param [in,out]	o   	A std::ostream to process.
+	 *    @param 		 	t   	A T to process.
+	 *    @param 		 	args	Variable arguments providing the arguments.
+	 */
 
-		void LogHelp(std::ostream& o, T t, Args... args)
-		{
-			LogHelp(o, t);
-			LogHelp(o, args...);
-		}
+	void LogHelp(std::ostream& o, T t, Args... args)
+	{
+		LogHelp(o, t);
+		LogHelp(o, args...);
+	}
 
-		/* The log severity strings[ 4] */
-		std::string LogSeverityStrings[4];
-		/* The log mut */
-		std::mutex logMut;
+	/* The log severity strings[ 4] */
+	std::string LogSeverityStrings[4];
+	/* The log mut */
+	std::mutex logMut;
 
 
-		/* A log i. */
-		typedef struct LogI {
+	/* A log i. */
+	typedef struct LogI {
 
-			/* The message */
-			std::string msg;
-			/* The severity */
-			LogSeverity severity;
-			/* The lune number */
-			int luneNumber;
-			/* Filename of the file */
-			const char* filename;
+		/* The message */
+		std::string msg;
+		/* The severity */
+		LogSeverity severity;
+		/* The lune number */
+		int luneNumber;
+		/* Filename of the file */
+		const char* filename;
 
 		// .
-		}LogI;
+	}LogI;
 
-		/* The storage */
-		std::deque<LogI> storage;
+	/* The storage */
+	std::deque<LogI> storage;
 
-		/*
-		 *    Setters for logging messages. Interfaced by macros.
-		 *    @param [in,out]	oss 	The oss.
-		 *    @param 		 	file	The file.
-		 *    @param 		 	line	The line.
-		 */
+	/*
+	 *    Setters for logging messages. Interfaced by macros.
+	 *    @param [in,out]	oss 	The oss.
+	 *    @param 		 	file	The file.
+	 *    @param 		 	line	The line.
+	 */
 
-		void WriteInfo(std::ostream& oss, const char* file, int line);
+	void WriteInfo(std::ostream& oss, const char* file, int line);
 
 
-		//Logs a message of the specified type
-		template<typename... Args>
+	//Logs a message of the specified type
+	template<typename... Args>
 
-		/*
-		 *    Logs.
-		 *    @param	logType	Type of the log.
-		 *    @param	file   	The file.
-		 *    @param	line   	The line.
-		 *    @param	args   	Variable arguments providing the arguments.
-		 */
+	/*
+	 *    Logs.
+	 *    @param	logType	Type of the log.
+	 *    @param	file   	The file.
+	 *    @param	line   	The line.
+	 *    @param	args   	Variable arguments providing the arguments.
+	 */
 
-		void Log(LogSeverity logType, const char* file, int line, Args... args)
-		{
-			std::ostringstream oss;
-			WriteInfo(oss, file, line);
-			Logger::LogHelp(oss, args...);
-			Logger::logMut.lock();
-			Logger::storage.push_back({ oss.str() ,logType ,line,file });
-			Logger::logMut.unlock();
-			std::cout << oss.str() << std::endl; //temporary
-		}
-	
+	void Log(LogSeverity logType, const char* file, int line, Args... args)
+	{
+		std::ostringstream oss;
+		WriteInfo(oss, file, line);
+		Logger::LogHelp(oss, args...);
+		Logger::logMut.lock();
+		Logger::storage.push_back({ oss.str() ,logType ,line,file });
+		Logger::logMut.unlock();
+		std::cout << oss.str() << std::endl; //temporary
+	}
+
 
 	/*
 	 *    Get the next available string to write. Empty string if none written.
