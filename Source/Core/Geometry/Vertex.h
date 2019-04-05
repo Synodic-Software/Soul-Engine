1ac16c4998a462b89a89377e26aa390d26642ea3
Asher Norland <asher.norland@gmail.com>
2019-04-05 00:22:32 -0400

    Remove Vertex Cruft

--- Source/Core/Geometry/Vertex.h
+++ Source/Core/Geometry/Vertex.h
@@ -14,13 +14,12 @@ public:
 	Vertex() = default;
 	~Vertex() = default;
 
-	Vertex(const Vertex &) = default;
-	Vertex(Vertex &&) noexcept = default;
+	Vertex(const Vertex&) = default;
+	Vertex(Vertex&&) noexcept = default;
 
-	Vertex& operator=(const Vertex &) = default;
-	Vertex& operator=(Vertex &&) noexcept = default;
+	Vertex& operator=(const Vertex&) = default;
+	Vertex& operator=(Vertex&&) noexcept = default;
 
-	bool operator==(const Vertex& other) const;
 
 	glm::vec3 position;
 	glm::vec2 textureCoord;
@@ -30,16 +29,3 @@ public:
 	uint object;
 
 };
-
-namespace std {
-	template<> struct hash<Vertex> {
-		size_t operator()(Vertex const& vertex) const {
-			return (hash<float>()(vertex.position.x) ^
-				hash<float>()(vertex.position.y) ^
-				hash<float>()(vertex.position.z));
-
-		}
-
-	};
-
-}
