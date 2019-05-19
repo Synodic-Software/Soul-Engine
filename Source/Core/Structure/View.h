dd699f49acf8ba67a3a3d43a3439ad6822a7403c
Asher Norland <asher.norland@gmail.com>
2019-05-18 23:41:30 -0400

    Implemented Basic View

--- Source/Core/Structure/View.h
+++ Source/Core/Structure/View.h
@@ -1,17 +1,62 @@
 #pragma once
 
+#include <functional>
+#include <optional>
+
 template<class T>
-class View{
+class View<T> {
 
-	// TODO: C++20 Concepts
-	static_assert(std::is_base_of<Contiguous, T>::value,
-		"The T parameter must be a subclass of Contiguous");
-
+	using size_type = size_t;
+	using function_type = std::function<size_type(size_type)>;
 
 public:
 
-	View() = default;
+	constexpr View();
+	constexpr View(T* ptr, size_type count);
 	virtual ~View() = default;
+
+	T& operator[](size_type);
+
+	void Map(function_type&& function);
+
+
+private:
+
+	// TODO: std::unique_function C++20
+	std::optional<function_type> indexMap_;
+
+	//TODO: replace with std::span C++20
+	const T* data_;
+	const size_type size_;
 
 
 };
+
+template<class T>
+constexpr View<T>::View(): data_(nullptr), size_(0)
+{
+}
+
+template<class T>
+constexpr View<T>::View(T* ptr, size_type count): data_(ptr), size_(count)
+{
+}
+
+template<class T>
+T& View<T>::operator[](size_type index)
+{
+	if (!indexMap_.has_value()) {
+		return data_[index];
+	}
+	
+	return data_[*indexMap_(index)];
+
+}
+
+template<class T>
+void View<T>::Map(function_type&& function)
+{
+
+	indexMap_.emplace(std::move(myFunction))
+
+}
